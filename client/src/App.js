7d8abe0c95ff5871e15751d57474bed3294c839b
unknown <niharikamahajan47@gmail.com>
2022-11-12 23:40:41 +0530

    Sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
-import './App.css';
+import "./App.css";
+import SideMenu, { menuItems } from "./components/SideMenu";
+import "./components/SideNavBar.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [inactive, setInactive] = useState(false);
+
   return (
     <div className="App">
-      hello from plinth
+      <Router>
+        <SideMenu
+          onCollapse={(inactive) => {
+            console.log(inactive);
+            setInactive(inactive);
+          }}
+        />
+
+        <div className={`container ${inactive ? "inactive" : ""}`}>
+          {}
+          {menuItems.map((menu, index) => (
+            <>
+              <Route key={menu.name} exact={menu.exact} path={menu.to}>
+                <h1>{menu.name}</h1>
+              </Route>
+              {menu.subMenus && menu.subMenus.length > 0
+                ? menu.subMenus.map((subMenu, i) => (
+                    <Route key={subMenu.name} path={subMenu.to}>
+                      <h1>{subMenu.name}</h1>
+                    </Route>
+                  ))
+                : null}
+            </>
+          ))}
+
+          {}
+        </div>
+      </Router>
     </div>
   );
 }
